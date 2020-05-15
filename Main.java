cbac4c4e6c33797f0582a7506ffb684aef91c01b
tatiana-polyakova <tanch88@mail.ru>
2020-05-15 10:32:32 +0300

    add files

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String [] args) {
+        float ticket = 143_567;
+        int coefficient = 20;
+        float result = ticket / coefficient;
+        System.out.println(result);
+    }
+}
